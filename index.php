2be1417020372a889d74936ea6ff065c8196b5c8
Richard Rodríguez Salazar <richardrodrsal@gmail.com>
2025-11-20 13:56:22 -0400

    Update index.php
    Cambio en index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   gtag('config', 'G-GR5YQEJPJZ');
 </script>
 
-	<title>StevieWonderLand - Posada Boutique. El Yaque, Margarita, Venezuela</title>
+	<title>original - StevieWonderLand - Posada Boutique. El Yaque, Margarita, Venezuela</title>
 	<meta name=description content=">>>Reservaciones - Bookings - Buchungen<<<: stevie.wonder.land@hotmail.de +58 424 8740741 +58 414 7905050 +58 414 3291300">
 	<meta name=keywords content="viaje a margarita, viaje a venezuela, turismo venezuela, turismo margarita, Posada Boutique margarita, Posada Boutique venezuela, hospedaje margarita, hospedaje venezuela, Posada Boutiquees margarita, Posada Boutiquees venezuela, turismo, tour, Posada Boutique, posada, isla, playa, caribe, nueva esparta, viaje, kite, kitesurf, windsurf, yaque, margarita, island, vacation, beach, caribbean, venezuela, ">
 	<meta charset="utf-8">
